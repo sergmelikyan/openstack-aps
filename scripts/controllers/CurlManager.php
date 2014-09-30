68fccd53f717744010075e568fd0d175a1e7156f
Jepi Humet <jepi.humet@mamasu.es>
2014-09-30 19:39:44 +0200

    Sprint 2 completed

--- scripts/controllers/CurlManager.php
+++ scripts/controllers/CurlManager.php
@@ -13,24 +13,53 @@ class CurlManager {
      * @return type
      */
     protected function send($url, $port, $api, $method, $parameters = array(), $headers = array()) {
+//        logme("SEND CURL", array($url, $port, $api, $method, $parameters, $headers));
         $url_send = $url . ":" . $port . "/" . $api;
-        $str_data = json_encode($parameters);
-        $headers[] = 'Content-Type: application/json';
-        $result = $this->sendPostData($url_send, $str_data, $headers, $method);
+        $headers[] = "Content-Type: application/json";
+        $result = $this->sendPostData($url_send, $parameters, $headers, $method);
+        logme("CURL RESULT", array($result));
         return json_decode($result);
     }
 
     private function sendPostData($url, $post, $headers, $method) {
         $ch = curl_init($url);
+        $file = fopen(getcwd() . '/controllers/curl_log.txt', 'w');
+        curl_setopt($ch, CURLOPT_STDERR, $file);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        if (count($post) > 0) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+        }
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $result = curl_exec($ch);
         if (curl_errno($ch)) { //There is one error            
             return curl_error($ch);
         }
+
+        $debugText = "=====================DEBUG================== <BR><BR>";
+
+        $err = curl_error($ch);
+        $debugText .= "<-- start err-><br>";
+        $debugText .= "<pre>" . print_r($err, TRUE) . "</pre>";
+        $debugText .= "<-- end err--><br>";
+
+        $debugText .= "<-- start headers-><br>";
+        $debugText .= "<pre>" . print_r($headers, TRUE) . "</pre>";
+        $debugText .= "<-- end headers--><br>";
+
+        $info = curl_getinfo($ch);
+        $debugText .= "<-- start info-><br>";
+        $debugText .= "<pre>" . print_r($info, TRUE) . "</pre>";
+        $debugText .= "<-- end info--><br>";
+
+        $debugText .= "<-- start result-><br>";
+        $debugText .= "<pre>" . print_r($result, TRUE) . "</pre>";
+        $debugText .= "<-- end result-><br>";
+        fputs($file, $debugText);
+        fclose($file);
+
         curl_close($ch);
         return $result;
     }
